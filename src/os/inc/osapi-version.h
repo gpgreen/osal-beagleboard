4b903facd49aa3c9cfacf329f0982b5e33759421
Greg Green <gpgreen@gmail.com>
2012-10-26 23:53:25 -0700

    Initial import of 3.4.1-release

---
+++ src/os/inc/osapi-version.h
@@ -0,0 +1,49 @@
+/************************************************************************
+** File:
+**   $Id: osapi-version.h 1.7 2012/01/17 16:04:29EST acudmore Exp  $
+**
+**      Copyright (c) 2004-2006, United States government as represented by the 
+**      administrator of the National Aeronautics Space Administration.  
+**      All rights reserved. This software was created at NASAs Goddard 
+**      Space Flight Center pursuant to government contracts.
+**
+**      This is governed by the NASA Open Source Agreement and may be used, 
+**      distributed and modified only pursuant to the terms of that agreement.
+**
+** Purpose: 
+**  The OSAL version numbers 
+**
+** Notes:
+**
+** $Log: osapi-version.h  $
+** Revision 1.7 2012/01/17 16:04:29EST acudmore 
+** Updated version to 3.4.1
+** Revision 1.6 2011/12/05 15:45:16EST acudmore 
+** Updated version label to 3.4.0
+** Revision 1.5 2011/06/20 14:06:02EDT acudmore 
+** Updated version label
+** Revision 1.4 2010/11/16 10:57:11EST acudmore 
+** Updated to version 3.2.0.0
+** Revision 1.3 2010/11/12 12:00:19EST acudmore 
+** replaced copyright character with (c) and added open source notice where needed.
+** Revision 1.2 2010/03/12 12:14:04EST acudmore 
+** Updated version ID to 3.1.0.0
+** Revision 1.1 2010/03/08 15:57:43EST acudmore 
+** Initial revision
+** Member added to project c:/MKSDATA/MKS-REPOSITORY/MKS-OSAL-REPOSITORY/src/os/inc/project.pj
+**
+*************************************************************************/
+#ifndef _osapi_version_h_
+#define _osapi_version_h_
+
+#define OS_MAJOR_VERSION (3)
+#define OS_MINOR_VERSION (4)
+#define OS_REVISION      (1)
+#define OS_MISSION_REV   (0)
+
+      
+#endif /* _osapi_version_h_ */
+
+/************************/
+/*  End of File Comment */
+/************************/
